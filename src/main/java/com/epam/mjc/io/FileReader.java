2896370b9b3481ef83d56ccc2ebd2403cbc0b27b
InDev <InDevRus@hotmail.com>
2023-03-22 22:01:21 +0500

    Refactored code

--- src/main/java/com/epam/mjc/io/FileReader.java
+++ src/main/java/com/epam/mjc/io/FileReader.java
@@ -3,23 +3,26 @@ package com.epam.mjc.io;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.IOException;
+import java.text.MessageFormat;
 import java.util.List;
 import java.util.Objects;
+import java.util.logging.Logger;
 import java.util.regex.Pattern;
 
 
 public class FileReader {
+    private static final Logger logger = Logger.getLogger(FileReader.class.getName());
     //I would rather create enum right here, but we haven't studied that yet.
     private static final String NAME_FIELD = "Name";
     private static final String AGE_FIELD = "Age";
     private static final String EMAIL_FIELD = "Email";
     private static final String PHONE_FIELD = "Phone";
-    private static final String fieldNamesPattern = String.join("|", List.of(NAME_FIELD, AGE_FIELD, EMAIL_FIELD, PHONE_FIELD));
+    private static final String FIELD_NAMES_PATTERN = String.join("|", List.of(NAME_FIELD, AGE_FIELD, EMAIL_FIELD, PHONE_FIELD));
     //Nevertheless, neither regular expressions nor String.split method were studied, so I've chosen the first option.
-    private static final Pattern dataPattern = Pattern.compile(String.format("(%s): (.+)", fieldNamesPattern));
+    private static final Pattern LINE_PATTERN = Pattern.compile(String.format("(%s): (.+)", FIELD_NAMES_PATTERN));
 
     private void updateProfileFromLine(Profile profile, String line) {
-        var matcher = dataPattern.matcher(line);
+        var matcher = LINE_PATTERN.matcher(line);
         if (!matcher.find()) {
             throw ProfileParseException.ofInvalidLine(line, null);
         }
@@ -50,6 +53,7 @@ public class FileReader {
                 profile.setPhone(Long.parseLong(value));
                 break;
             }
+            default:
         }
     }
 
@@ -61,8 +65,7 @@ public class FileReader {
                 updateProfileFromLine(profile, line);
             }
         } catch (IOException | ProfileParseException exception) {
-            System.err.println("There was an error during file reading:");
-            System.err.println(exception.getMessage());
+            logger.severe(MessageFormat.format("There was an error during file reading: {0}", exception.getMessage()));
         }
         return profile;
     }
